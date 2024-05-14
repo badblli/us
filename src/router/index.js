b050d860392e8eccd61afa9f544a295c56d6f14d
badblli <busenur.adibelli@netsolutionworld.com>
2024-05-14 11:54:31 +0300

    feat: Add Us page to the router and update CSS color
    The code changes add a new route for the "Us" page in the Vue router and import the corresponding component. Additionally, the CSS color for the body element is updated.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Us from '../views/Us.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,9 +13,6 @@ const router = createRouter({
     {
       path: '/us',
       name: 'us',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Us.vue')
     }
   ]
